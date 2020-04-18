12deddb5c8b6d38f4a7de29589773516b9b9f352
FredC94 <fcastel@gmail.com>
2020-04-18 19:25:10 +0200

    commit

---
+++ UpyLab/UpyLaB 5.19.py
@@ -0,0 +1,15 @@
+"""
+Enoncé:
+Écrire une fonction acrostiche qui reçoit en paramètre le nom d’un fichier et qui retourne
+la chaîne de caractères formée par les premières lettres de chaque ligne du fichier.
+
+Consignes:
+Dans cet exercice, il vous est demandé d’écrire seulement la fonction acrostiche. Le code que vous soumettez à UpyLaB doit donc comporter uniquement la définition de cette fonction, et ne fait en particulier aucun appel à input ou à print.
+Les fichiers utilisés par UpyLaB pour tester la fonction sont accessibles aux adresses suivantes :
+    https://upylab.ulb.ac.be/pub/data/acro1.txt
+    https://upylab.ulb.ac.be/pub/data/acro2.txt
+    https://upylab.ulb.ac.be/pub/data/acro3.txt
+
+"""
+def acrostiche(fichier):
+    
